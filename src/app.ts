ceef7de2159f1e2430b39046517a4caf42878056
sossw1 <sossw1@gmail.com>
2022-02-28 13:58:25 -0500

    serve basic html

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.json());
+app.use(express.static(publicDirectoryPath));
 
 export default app;
