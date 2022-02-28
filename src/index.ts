80b872e1c66e502adc078f3799d6bb784ed77aac
sossw1 <sossw1@gmail.com>
2022-02-28 14:34:16 -0500

    socket connections

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import chalk from 'chalk';
 import express from 'express';
+import http from 'http';
 import path from 'path';
+import { Server } from 'socket.io';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server);
+
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.json());
 app.use(express.static(publicDirectoryPath));
 
-app.listen(PORT, () => {
-  console.log('Server running on port', chalk.yellow(PORT));
+server.listen(PORT, () => {
+  console.log('Server running on PORT', chalk.yellow(PORT));
 });
