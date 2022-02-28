b5fe2691c5b0f44a8dd04da8b164f0aa8406c7c3
sossw1 <sossw1@gmail.com>
2022-02-28 14:00:37 -0500

    condensed app into index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-import app from './app';
 import chalk from 'chalk';
+import express from 'express';
+import path from 'path';
 
 const PORT = process.env.PORT || 3000;
+const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.json());
+app.use(express.static(publicDirectoryPath));
 
 app.listen(PORT, () => {
-  console.log('App listening on port', chalk.yellow(PORT));
+  console.log('Server running on port', chalk.yellow(PORT));
 });
