f8b7beac505af2271539a6e3aa479e10e0100a1f
sossw1 <sossw1@gmail.com>
2022-03-04 21:50:59 -0500

    function to remove user

--- src/utils/users.ts
+++ src/utils/users.ts
@@ -31,4 +31,11 @@ const addUser = ({ id, username, room }: IUser) => {
   return { user };
 };
 
-export { users, addUser };
+const removeUser = (id: number) => {
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+export { users, addUser, removeUser };
