0f057536526e197e78ea88cebbc41dab1cbb51b5
sossw1 <sossw1@gmail.com>
2022-03-09 20:50:42 -0500

    user model

---
+++ src/models/User.ts
@@ -0,0 +1,52 @@
+import {
+  Document,
+  model,
+  Model,
+  Schema,
+  SchemaDefinitionProperty
+} from 'mongoose';
+
+export interface IUser {
+  username: string;
+  password: string;
+  email: string;
+}
+
+export interface IUserDoc extends IUser, Document {}
+
+enum PropertyNames {
+  USERNAME = 'username',
+  PASSWORD = 'password',
+  EMAIL = 'email'
+}
+
+export interface IUserModel extends Model<IUserDoc> {
+  PropertyNames: typeof PropertyNames;
+}
+
+const UserSchemaFields: Record<keyof IUser, SchemaDefinitionProperty> = {
+  username: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true
+  }
+};
+
+const UserSchema = new Schema(UserSchemaFields, {
+  timestamps: true
+});
+
+const UserCollection = model<IUserDoc, IUserModel>('users', UserSchema);
+
+export default UserCollection;
