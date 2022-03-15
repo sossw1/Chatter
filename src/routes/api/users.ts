a6e4484863f877acc624f6d8ff4c2a6ee53f9cc3
sossw1 <sossw1@gmail.com>
2022-03-14 22:57:32 -0400

    POST user

---
+++ src/routes/api/users.ts
@@ -0,0 +1,21 @@
+import UserCollection, { IUser, IUserDoc } from '../../models/User';
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/api/users', async (req, res) => {
+  try {
+    const user: IUser = {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    };
+    const userDocument: IUserDoc = new UserCollection(user);
+    await userDocument.save();
+    res.status(201).send({ user: userDocument });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+export default router;
