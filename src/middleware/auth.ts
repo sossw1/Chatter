45b87c50e34122459138a8311551001a4eaf7390
sossw1 <sossw1@gmail.com>
2022-03-15 12:18:28 -0400

    authentication middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,36 @@
+import jwt, { Secret } from 'jsonwebtoken';
+import User from '../models/User';
+
+import { Request, Response, NextFunction } from 'express';
+
+const auth = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) {
+      return res.status(401).send({ error: 'Not authenticated' });
+    } else {
+      const secret: Secret = process.env.JWT_SECRET || 'd^e#f@a*u$l%t';
+      const decoded = await jwt.verify(token, secret);
+      if (typeof decoded !== 'string') {
+        const user = await User.findOne({
+          _id: decoded._id,
+          'tokens.token': token
+        });
+
+        if (!user) {
+          throw new Error();
+        }
+
+        req.token = token;
+        req.user = user;
+        next();
+      } else {
+        return res.status(401).send({ error: 'Not authenticated' });
+      }
+    }
+  } catch (error) {
+    return res.status(401).send({ error: 'Not authenticated' });
+  }
+};
+
+export default auth;
