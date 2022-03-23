ce814941ec8ebea468e2e19ca9337fe176b5f4af
sossw1 <sossw1@gmail.com>
2022-03-23 19:30:00 -0400

    route to signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Login from './Login';
+import Signup from './Signup';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
