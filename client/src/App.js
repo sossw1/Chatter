75deb373db104a8a32af40c0ae1f5b291d999e5d
sossw1 <sossw1@gmail.com>
2022-03-22 21:41:02 -0400

    basic router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<></>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
