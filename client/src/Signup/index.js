23b8a83803f258af104a148933cc1efeb07ceb83
sossw1 <sossw1@gmail.com>
2022-03-23 19:29:38 -0400

    signup component

---
+++ client/src/Signup/index.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+export default function Signup() {
+  return (
+    <div class='centered-form'>
+      <div class='centered-form__box'>
+        <h1>Sign up!</h1>
+        <form action='/rooms.html'>
+          <label>Username</label>
+          <input type='text' name='username' placeholder='Username' required />
+          <label>Email</label>
+          <input type='text' name='email' placeholder='Email' required />
+          <label>Password</label>
+          <input type='password' name='password' placeholder='Password' required />
+          <button>Create Account</button>
+        </form>
+        <p>Already have an account? Log in <Link to='/'>here</Link></p>
+      </div>
+    </div>
+  )
+}
