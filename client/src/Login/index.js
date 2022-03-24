ad182f846d9b163b94a63b47f0a02a2a2316c73d
sossw1 <sossw1@gmail.com>
2022-03-24 14:13:45 -0400

    fixed input change handling

--- client/src/Login/index.js
+++ client/src/Login/index.js
@@ -5,15 +5,23 @@ export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  }
+
   return (
     <div className='centered-form'>
       <div className='centered-form__box'>
         <h1>Login</h1>
         <form>
           <label>Username</label>
-          <input type='text' name='username' placeholder='Username' onChange={setUsername} required />
+          <input type='text' name='username' placeholder='Username' onChange={handleUsername} required />
           <label>Password</label>
-          <input type='password' name='password' placeholder='Password' onChange={setPassword} required />
+          <input type='password' name='password' placeholder='Password' onChange={handlePassword} required />
           <button>Login</button>
         </form>
         <p>Don't have an account? Sign up <Link to='/signup'>here</Link></p>
