ee968b634fa363b68c07aaaaed555a4407bec0a1
sossw1 <sossw1@gmail.com>
2022-03-25 13:48:10 -0400

    track current authenticated user

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,15 @@ import Login from './Login';
 import Signup from './Signup';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState({});
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Login />} />
+        <Route path='/' element={<Login setUser={setUser} />} />
         <Route path='/signup' element={<Signup />} />
       </Routes>
     </BrowserRouter>
