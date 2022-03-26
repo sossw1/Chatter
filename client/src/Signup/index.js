25270ddfea87c8a72cfefe0fafb217c54ba0638b
sossw1 <sossw1@gmail.com>
2022-03-26 16:36:49 -0400

    track state of form fields

--- client/src/Signup/index.js
+++ client/src/Signup/index.js
@@ -1,17 +1,38 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Signup() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = (e) => {
+    switch (e.target.id) {
+      case 'username':
+        setUsername(e.target.value);
+        break;
+      case 'email':
+        setEmail(e.target.value);
+        break;
+      case 'password':
+        setPassword(e.target.value);
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <div className='centered-form'>
       <div className='centered-form__box'>
         <h1>Sign up!</h1>
-        <form action='/rooms.html'>
+        <form>
           <label>Username</label>
-          <input type='text' name='username' placeholder='Username' required />
+          <input id='username' type='text' name='username' placeholder='Username' onChange={handleChange} required />
           <label>Email</label>
-          <input type='text' name='email' placeholder='Email' required />
+          <input id='email' type='text' name='email' placeholder='Email' onChange={handleChange} required />
           <label>Password</label>
-          <input type='password' name='password' placeholder='Password' required />
+          <input id='password' type='password' name='password' placeholder='Password' onChange={handleChange} required />
           <button>Create Account</button>
         </form>
         <p>Already have an account? Log in <Link to='/'>here</Link></p>
