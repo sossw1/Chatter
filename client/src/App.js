b5a4df1284b786c445794ce7fb690edb41c5186c
sossw1 <sossw1@gmail.com>
2022-03-29 16:49:48 -0400

    AuthProvider and removed props

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,22 @@ import './App.css';
 import Login from './Login';
 import Signup from './Signup';
 import Rooms from './Rooms';
+import { AuthProvider } from './Providers/auth';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
 
 function App() {
-  const [user, setUser] = useState({});
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login setUser={setUser} />} />
-        <Route path='/signup' element={<Signup setUser={setUser} />} />
-        <Route path='/rooms' element={<Rooms rooms={user.rooms} />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/rooms' element={<Rooms />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
