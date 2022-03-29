08f1c7daca4e97a1baaf6f70814d2b4c05cc89aa
sossw1 <sossw1@gmail.com>
2022-03-29 17:00:00 -0400

    require auth for /rooms

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,25 @@ import './App.css';
 import Login from './Login';
 import Signup from './Signup';
 import Rooms from './Rooms';
-import { AuthProvider } from './Providers/auth';
+import { AuthProvider, RequireAuth } from './Providers/auth';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
-          <Route path='/rooms' element={<Rooms />} />
+          <Route
+            path='/rooms'
+            element={
+              <RequireAuth>
+                <Rooms />
+              </RequireAuth>
+            }
+          ></Route>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
