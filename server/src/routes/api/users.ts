1a35737ca07fe15c5822169ba4e51694acf39400
sossw1 <sossw1@gmail.com>
2022-04-02 16:36:13 -0400

    send status on bad login

--- server/src/routes/api/users.ts
+++ server/src/routes/api/users.ts
@@ -54,7 +54,7 @@ router.post('/api/users/login', async (req, res) => {
     const token = await user.generateAuthToken();
     res.send({ user, token });
   } catch (error) {
-    res.status(400).send(error);
+    res.sendStatus(400);
   }
 });
 
