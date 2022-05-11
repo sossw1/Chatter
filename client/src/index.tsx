eae3fac4434d796adfc9263ba1b8b964630421b7
sossw1 <sossw1@gmail.com>
2022-05-11 19:20:18 -0400

    remade react app with ts

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
+const root = document.getElementById('root')! as HTMLElement;
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  root
 );
