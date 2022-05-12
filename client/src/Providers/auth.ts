e2eec70c6f1788d01096482d10ce7c59fc6397cc
sossw1 <sossw1@gmail.com>
2022-05-11 21:18:45 -0400

    Auth method getUserWithToken

--- client/src/Providers/auth.ts
+++ client/src/Providers/auth.ts
@@ -1,3 +1,9 @@
+interface User {
+  username: string;
+  password: string;
+  email: string;
+}
+
 class Auth {
   private isAuthenticated: boolean;
   private static instance: Auth;
@@ -12,6 +18,26 @@ class Auth {
     }
     this.instance = new Auth();
     return this.instance;
+  }
+
+  async getUserWithToken() {
+    const stringToken = localStorage.getItem('token');
+    if (!stringToken) return;
+    const parsedToken: User = JSON.parse(stringToken);
+    const url = '/api/users/me';
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${parsedToken}`,
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    });
+    if (!response || !response.ok) return;
+    const user: User = await response.json();
+    if (!user) return;
+    this.isAuthenticated = true;
+    return user;
   }
 }
 
