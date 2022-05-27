4b1c1a3c48a11980e003e250b844aa35484e0b6d
sossw1 <sossw1@gmail.com>
2022-05-27 16:15:17 -0400

    remove unused 'field' property

--- client/src/validation/signup.ts
+++ client/src/validation/signup.ts
@@ -1,7 +1,6 @@
 import validator from 'validator';
 
 export interface ValidationError {
-  field: 'username' | 'email' | 'password' | 'password2';
   error: string;
 }
 
@@ -13,34 +12,29 @@ export function validate(
 ) {
   const errors: ValidationError[] = [];
   if (validator.isEmpty(username)) {
-    errors.push({ field: 'username', error: 'Username is required' });
+    errors.push({ error: 'Username is required' });
   }
   if (!validator.isLength(username, { min: 8, max: 20 })) {
-    errors.push({
-      field: 'username',
-      error: 'Username must be between 8 and 20 characters'
-    });
+    errors.push({ error: 'Username must be between 8 and 20 characters' });
   }
   if (validator.isEmpty(email)) {
-    errors.push({ field: 'email', error: 'Email Address is required' });
+    errors.push({ error: 'Email Address is required' });
   }
   if (!validator.isEmail(email)) {
     errors.push({
-      field: 'email',
       error: 'Invalid email address'
     });
   }
   if (validator.isEmpty(password)) {
-    errors.push({ field: 'password', error: 'Password is required' });
+    errors.push({ error: 'Password is required' });
   }
   if (!validator.isLength(password, { min: 8, max: 20 })) {
     errors.push({
-      field: 'password',
       error: 'Password must be between 8 and 20 characters'
     });
   }
   if (!validator.equals(password, password2)) {
-    errors.push({ field: 'password2', error: 'Passwords must match' });
+    errors.push({ error: 'Passwords must match' });
   }
 
   const isValid = errors.length === 0;
