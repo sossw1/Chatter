c95e77e6752dd85a345dfb5e5830026b0bf44d97
sossw1 <sossw1@gmail.com>
2022-05-31 23:57:16 -0400

    authenticated route /chat, placeholder component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './Providers/auth';
+import { AuthProvider, RequireAuth } from './Providers/auth';
 import theme from './Providers/theme';
 import Login from './Components/Login';
 import SignUp from './Components/SignUp';
+import Chat from './Components/Chat';
 
 function App() {
   return (
@@ -16,6 +17,14 @@ function App() {
             <Routes>
               <Route path='/' element={<Login />} />
               <Route path='/signup' element={<SignUp />} />
+              <Route
+                path='/chat'
+                element={
+                  <RequireAuth>
+                    <Chat />
+                  </RequireAuth>
+                }
+              />
             </Routes>
           </BrowserRouter>
         </AuthProvider>
