96acf56964d844b917a9e6273dc26f452e3d3ad4
sossw1 <sossw1@gmail.com>
2022-06-07 21:52:55 -0400

    paper on grey background

--- client/src/Components/Chat.tsx
+++ client/src/Components/Chat.tsx
@@ -1,3 +1,19 @@
+import { Box, Paper, useMediaQuery } from '@mui/material';
+import theme from '../Providers/theme';
+
 export default function Chat() {
-  return <>Chat</>;
+  const xsDown = useMediaQuery(theme.breakpoints.down('sm'));
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: theme.palette.grey[200],
+        p: '1rem'
+      }}
+    >
+      <Paper
+        sx={{ height: `calc(100vh - ${xsDown ? '5.5rem' : '6rem'})` }}
+      ></Paper>
+    </Box>
+  );
 }
