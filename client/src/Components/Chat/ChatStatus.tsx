764fd97b32b8ee561257dc59ac34f9959a80b97e
sossw1 <sossw1@gmail.com>
2022-06-27 20:53:21 -0400

    status circle icon

--- client/src/Components/Chat/ChatStatus.tsx
+++ client/src/Components/Chat/ChatStatus.tsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
 import { Avatar, Box, Grid, Typography, useMediaQuery } from '@mui/material';
+import { Circle } from '@mui/icons-material';
 import { useAuth } from '../../Providers/auth';
 import theme from '../../Providers/theme';
 
 export default function ChatStatus() {
+  const [statusColor, setStatusColor] = useState<
+    'success' | 'warning' | 'error'
+  >('success');
+
   const smDown = useMediaQuery(theme.breakpoints.down('md'));
   const mdDown = useMediaQuery(theme.breakpoints.down('lg'));
 
@@ -26,20 +32,34 @@ export default function ChatStatus() {
           <Avatar sx={{ width: '2.5rem', height: '2.5rem' }} />
         </Grid>
         <Grid item>
-          <Typography
-            variant='subtitle1'
-            sx={{
-              lineHeight: '1.5rem',
-              fontWeight: 300,
-              width: mdDown ? (smDown ? '10rem' : '15rem') : '25rem',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-              whiteSpace: 'nowrap'
-            }}
-          >
-            {username}
-          </Typography>
-          <Typography variant='subtitle2'>status</Typography>
+          <Grid container direction='column'>
+            <Typography
+              variant='subtitle1'
+              sx={{
+                lineHeight: '1.5rem',
+                fontWeight: 300,
+                width: mdDown ? (smDown ? '10rem' : '15rem') : '25rem',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                whiteSpace: 'nowrap'
+              }}
+            >
+              {username}
+            </Typography>
+            <Grid item>
+              <Grid container direction='row'>
+                <Grid item sx={{ mr: '0.25rem' }}>
+                  <Circle
+                    sx={{ width: '0.75rem', height: '0.75rem' }}
+                    color={statusColor}
+                  />
+                </Grid>
+                <Grid item>
+                  <Typography variant='subtitle2'>Online</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </Box>
