6d161fe1159eefb7e461626a109e66babe2ed939
sossw1 <sossw1@gmail.com>
2022-06-28 21:32:53 -0400

    list item buttons

--- client/src/Components/Chat/ChatList.tsx
+++ client/src/Components/Chat/ChatList.tsx
@@ -5,6 +5,7 @@ import {
   List,
   ListItem,
   ListItemAvatar,
+  ListItemButton,
   ListItemText,
   Typography,
   useMediaQuery
@@ -16,87 +17,91 @@ export default function ChatList() {
   const mdDown = useMediaQuery(theme.breakpoints.down('lg'));
 
   return (
-    <Box sx={{ padding: '1.5rem .5rem .5rem' }}>
+    <Box sx={{ padding: '1.5rem .75rem .75rem' }}>
       <Grid container direction='column'>
-        <Grid item>
-          <Typography variant='h5' color='primary' sx={{ ml: '.75rem' }}>
+        <Grid item sx={{ width: '100%' }}>
+          <Typography variant='h5' color='primary' sx={{ ml: '1rem' }}>
             Chats
           </Typography>
           <List sx={{ mb: '1rem' }}>
-            <ListItem sx={{ padding: '.5rem .75rem' }}>
-              <ListItemAvatar>
-                <Avatar />
-              </ListItemAvatar>
-              <ListItemText
-                primary={
-                  <Typography
-                    sx={{
-                      width: mdDown ? (smDown ? '8rem' : '13rem') : '23rem',
-                      overflow: 'hidden',
-                      textOverflow: 'ellipsis',
-                      whiteSpace: 'nowrap'
-                    }}
-                  >
-                    username/roomname
-                  </Typography>
-                }
-                secondary={
-                  <Typography
-                    variant='body2'
-                    color='text.secondary'
-                    sx={{
-                      width: mdDown ? (smDown ? '8rem' : '13rem') : '23rem',
-                      overflow: 'hidden',
-                      textOverflow: 'ellipsis',
-                      whiteSpace: 'nowrap'
-                    }}
-                  >
-                    Laboris est sint velit Lorem adipisicing et. Pariatur
-                    eiusmod labore esse excepteur in veniam enim ipsum sunt.
-                  </Typography>
-                }
-              ></ListItemText>
+            <ListItem sx={{ p: 0 }}>
+              <ListItemButton>
+                <ListItemAvatar>
+                  <Avatar />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={
+                    <Typography
+                      sx={{
+                        width: mdDown ? (smDown ? '8rem' : '13rem') : '23rem',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        whiteSpace: 'nowrap'
+                      }}
+                    >
+                      username/roomname
+                    </Typography>
+                  }
+                  secondary={
+                    <Typography
+                      variant='body2'
+                      color='text.secondary'
+                      sx={{
+                        width: mdDown ? (smDown ? '8rem' : '13rem') : '23rem',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        whiteSpace: 'nowrap'
+                      }}
+                    >
+                      Laboris est sint velit Lorem adipisicing et. Pariatur
+                      eiusmod labore esse excepteur in veniam enim ipsum sunt.
+                    </Typography>
+                  }
+                ></ListItemText>
+              </ListItemButton>
             </ListItem>
           </List>
         </Grid>
         <Grid item>
-          <Typography variant='h5' color='primary' sx={{ ml: '.75rem' }}>
+          <Typography variant='h5' color='primary' sx={{ ml: '1rem' }}>
             Friends
           </Typography>
           <List>
-            <ListItem sx={{ padding: '.5rem .75rem' }}>
-              <ListItemAvatar>
-                <Avatar />
-              </ListItemAvatar>
-              <ListItemText
-                primary={
-                  <Typography
-                    sx={{
-                      width: mdDown ? (smDown ? '8rem' : '13rem') : '23rem',
-                      overflow: 'hidden',
-                      textOverflow: 'ellipsis',
-                      whiteSpace: 'nowrap'
-                    }}
-                  >
-                    username
-                  </Typography>
-                }
-                secondary={
-                  <Typography
-                    variant='body2'
-                    color='text.secondary'
-                    sx={{
-                      width: mdDown ? (smDown ? '8rem' : '13rem') : '23rem',
-                      overflow: 'hidden',
-                      textOverflow: 'ellipsis',
-                      whiteSpace: 'nowrap'
-                    }}
-                  >
-                    Duis do consequat est ut ex mollit sunt fugiat
-                    pariatur.Veniam sit id do deserunt et.
-                  </Typography>
-                }
-              ></ListItemText>
+            <ListItem sx={{ p: 0 }}>
+              <ListItemButton>
+                <ListItemAvatar>
+                  <Avatar />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={
+                    <Typography
+                      sx={{
+                        width: mdDown ? (smDown ? '8rem' : '13rem') : '23rem',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        whiteSpace: 'nowrap'
+                      }}
+                    >
+                      username
+                    </Typography>
+                  }
+                  secondary={
+                    <Typography
+                      variant='body2'
+                      color='text.secondary'
+                      sx={{
+                        width: mdDown ? (smDown ? '8rem' : '13rem') : '23rem',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        whiteSpace: 'nowrap'
+                      }}
+                    >
+                      Duis do consequat est ut ex mollit sunt fugiat
+                      pariatur.Veniam sit id do deserunt et.
+                    </Typography>
+                  }
+                ></ListItemText>
+              </ListItemButton>
             </ListItem>
           </List>
         </Grid>
