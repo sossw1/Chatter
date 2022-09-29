fce47a8aa042980d8a2a22a24b27acceceac0a9e
sossw1 <sossw1@gmail.com>
2022-09-28 21:09:35 -0400

    refactor get route

--- src/routes/api/rooms.ts
+++ src/routes/api/rooms.ts
@@ -3,19 +3,12 @@ import express from 'express';
 import auth from '../../middleware/auth';
 import Filter from 'bad-words';
 import UserCollection from '../../models/User';
+import inRoom from '../../middleware/inRoom';
 
 const router = express.Router();
 
-router.get('/api/rooms/:id', auth, async (req, res) => {
-  try {
-    const roomId = req.params.id;
-    const room = await RoomCollection.findById(roomId);
-    if (!room) return res.sendStatus(404);
-    if (!room.users.includes(req.user.username)) return res.sendStatus(404);
-    res.status(200).send(room);
-  } catch (error) {
-    res.status(400).send({ error });
-  }
+router.get('/api/rooms/:roomId', auth, inRoom, async (req, res) => {
+  res.status(200).send(req.room);
 });
 
 router.post('/api/rooms', auth, async (req, res) => {
