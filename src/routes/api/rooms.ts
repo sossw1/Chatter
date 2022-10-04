80022e2ab917710d93f3894ea22b0421e0ad9eff
sossw1 <sossw1@gmail.com>
2022-10-03 21:34:55 -0400

    comments and spacing for readability

--- src/routes/api/rooms.ts
+++ src/routes/api/rooms.ts
@@ -7,9 +7,13 @@ import inRoom from '../../middleware/inRoom';
 
 const router = express.Router();
 
+// Get room by id
+
 router.get('/api/rooms/:roomId', auth, inRoom, async (req, res) => {
   res.status(200).send(req.room);
 });
+
+// Create a room
 
 router.post('/api/rooms', auth, async (req, res) => {
   try {
@@ -18,14 +22,20 @@ router.post('/api/rooms', auth, async (req, res) => {
       users: req.body.users,
       messages: []
     };
+
     if (!room.users.includes(req.user.username))
       return res
         .status(400)
         .send({ error: 'Room users must contain own username' });
+
     const filter = new Filter();
     if (filter.isProfane(room.name))
       return res.status(400).send({ error: 'Room name fails profanity check' });
-    const otherUsers = room.users.filter((user) => user !== req.user.username);
+
+    // Ensure all users exist
+    const otherUsers: string[] = room.users.filter(
+      (user) => user !== req.user.username
+    );
     for (let i = 0; i < otherUsers.length; i++) {
       const dbUser = await UserCollection.findOne({ username: otherUsers[i] });
       if (!dbUser)
@@ -33,40 +43,53 @@ router.post('/api/rooms', auth, async (req, res) => {
           .status(404)
           .send({ error: `User '${otherUsers[i]}' not found` });
     }
+
     const roomDocument: IRoomDoc = new RoomCollection(room);
     await roomDocument.save();
+
     res.status(201).send(roomDocument);
   } catch (error) {
     res.status(400).send(error);
   }
 });
 
+// Update room name
+
 router.patch('/api/rooms/:roomId', auth, inRoom, async (req, res) => {
   try {
     const { room } = req;
     const name: string = req.body.name;
+
     if (!name && name !== '')
       return res.status(400).send({ error: 'Invalid updates' });
+
     room.name = name;
     await room.save();
+
     res.sendStatus(200);
   } catch (error) {
     res.status(400).send({ error });
   }
 });
 
+// Add user to room
+
 router.patch('/api/rooms/:roomId/add', auth, inRoom, async (req, res) => {
   try {
     const newRoomMember: string = req.body.username;
     if (!newRoomMember)
       return res.status(400).send({ error: 'Invalid updates' });
+
     const { room } = req;
     if (room.users.includes(newRoomMember))
       return res.status(400).send({ error: 'User already member of room' });
+
     const user = await UserCollection.findOne({ username: newRoomMember });
     if (!user) return res.sendStatus(404);
+
     room.users.push(newRoomMember);
     await room.save();
+
     res.sendStatus(200);
   } catch (error) {
     res.status(400).send({ error });
