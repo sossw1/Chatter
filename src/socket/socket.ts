8bb62613e440036aca13307dbe1dfb49149d0b13
sossw1 <sossw1@gmail.com>
2022-11-28 21:10:37 -0500

    remove middleware and log

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -4,15 +4,5 @@ import { Server } from 'socket.io';
 export const setupSocketIO = (server: http.Server) => {
   const io = new Server(server);
 
-  io.use((socket, next) => {
-    const token = socket.handshake.auth.token;
-
-    // authenticate token
-
-    next();
-  });
-
-  io.on('connection', (socket) => {
-    console.log(socket.id, ' connected');
-  });
+  io.on('connection', (socket) => {});
 };
