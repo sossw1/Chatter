e29f6506662c8ba60c30f9d70eb5c6d13eb9981c
sossw1 <sossw1@gmail.com>
2022-11-29 20:30:34 -0500

    on connection store socketId

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,17 +1,23 @@
 import http from 'http';
 import { Server } from 'socket.io';
-import { IUserDoc } from '../models/User';
+import UserCollection, { IUserDoc } from '../models/User';
 
 export const setupSocketIO = (server: http.Server) => {
   const io = new Server(server);
   let user: IUserDoc | null;
 
   io.on('connection', (socket) => {
-    socket.on('user-data', (userData: IUserDoc) => {
+    socket.on('user-data', async (userData: IUserDoc) => {
       user = userData;
 
       for (let room of user.rooms) {
         socket.join(room + '');
+      }
+
+      const userDocument = await UserCollection.findById(user._id);
+      if (userDocument) {
+        userDocument.socketIds.push(socket.id);
+        await userDocument.save();
       }
     });
   });
