93334f32603a11a472b6e8fd90a443355e1fabc6
sossw1 <sossw1@gmail.com>
2023-01-21 20:56:59 -0500

    pull token and check if empty

--- client/src/Components/Friends/Friends.tsx
+++ client/src/Components/Friends/Friends.tsx
@@ -17,7 +17,8 @@ export default function Friends() {
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const username = event.currentTarget.querySelector('input')?.value;
-    if (!username) {
+    const token = localStorage.getItem('token');
+    if (!username || !token) {
       event.currentTarget.reset();
       return;
     }
