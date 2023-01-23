6b7f1bb98780f1daab809fe2e0e6feb2bff85755
sossw1 <sossw1@gmail.com>
2023-01-23 17:02:42 -0500

    pull friend request data from authenticated user

--- client/src/Components/Friend/Friend.tsx
+++ client/src/Components/Friend/Friend.tsx
@@ -1,13 +1,18 @@
 import { Box } from '@mui/material';
 import FriendRequest from './FriendRequest';
 import FriendRequestList from './FriendRequestList';
+import { useAuth } from '../../Providers/auth';
 
 export default function Friend() {
+  const { user } = useAuth();
+  let friendRequests: string[] = [];
+  if (user) friendRequests = user.friendInvites;
+
   return (
     <Box display='flex' flexDirection='row' p='2rem'>
       <Box display='flex' flexDirection='column'>
         <FriendRequest />
-        <FriendRequestList />
+        <FriendRequestList friendRequests={friendRequests} />
       </Box>
     </Box>
   );
--- client/src/Components/Friend/FriendRequestList.tsx
+++ client/src/Components/Friend/FriendRequestList.tsx
@@ -10,7 +10,11 @@ import {
 import { Check, Close } from '@mui/icons-material';
 import theme from '../../Providers/theme';
 
-export default function FriendRequestList() {
+interface Props {
+  friendRequests: string[];
+}
+
+export default function FriendRequestList({ friendRequests }: Props) {
   return (
     <Box display='flex' flexDirection='column'>
       <Typography variant='h3' mb='1.5rem'>
@@ -18,26 +22,32 @@ export default function FriendRequestList() {
       </Typography>
       <Divider />
       <List>
-        <ListItem>
-          <Box
-            display='flex'
-            flexDirection='row'
-            alignItems='center'
-            p='0.25rem 0'
-          >
-            <Avatar sx={{ width: '2.5rem', height: '2.5rem', mr: '1.5rem' }} />
-            <Typography variant='h5' mr='1rem'>
-              asdfasdf3
-            </Typography>
-            <Button sx={{ color: theme.palette.success.main }}>
-              <Check />
-            </Button>
-            <Button sx={{ color: theme.palette.error.main }}>
-              <Close />
-            </Button>
-          </Box>
-        </ListItem>
-        <Divider />
+        {friendRequests.map((username) => (
+          <>
+            <ListItem>
+              <Box
+                display='flex'
+                flexDirection='row'
+                alignItems='center'
+                p='0.25rem 0'
+              >
+                <Avatar
+                  sx={{ width: '2.5rem', height: '2.5rem', mr: '1.5rem' }}
+                />
+                <Typography variant='h5' mr='1rem'>
+                  {username}
+                </Typography>
+                <Button sx={{ color: theme.palette.success.main }}>
+                  <Check />
+                </Button>
+                <Button sx={{ color: theme.palette.error.main }}>
+                  <Close />
+                </Button>
+              </Box>
+            </ListItem>
+            <Divider />
+          </>
+        ))}
       </List>
     </Box>
   );
