31b92afaa6bf5e3e66d93d46e6165daea0951fa5
sossw1 <sossw1@gmail.com>
2023-01-23 17:54:49 -0500

    buttons POST reply to friend request, check accepts and x declines

--- client/src/Components/Friend/FriendRequestList.tsx
+++ client/src/Components/Friend/FriendRequestList.tsx
@@ -15,6 +15,31 @@ interface Props {
 }
 
 export default function FriendRequestList({ friendRequests }: Props) {
+  const replyFriendRequest = async (username: string, accept: boolean) => {
+    const token = localStorage.getItem('token');
+    let parsedToken: string = '';
+    if (token) parsedToken = JSON.parse(token);
+
+    const url = '/api/users/friend/reply';
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + parsedToken,
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({ username, accept })
+    });
+
+    if (response.ok) {
+      if (accept) {
+      } else {
+      }
+    } else {
+      const data = await response.json();
+    }
+  };
+
   return (
     <Box display='flex' flexDirection='column'>
       <Typography variant='h3' mb='1.5rem'>
@@ -37,10 +62,16 @@ export default function FriendRequestList({ friendRequests }: Props) {
                 <Typography variant='h5' mr='1rem'>
                   {username}
                 </Typography>
-                <Button sx={{ color: theme.palette.success.main }}>
+                <Button
+                  sx={{ color: theme.palette.success.main }}
+                  onClick={() => replyFriendRequest(username, true)}
+                >
                   <Check />
                 </Button>
-                <Button sx={{ color: theme.palette.error.main }}>
+                <Button
+                  sx={{ color: theme.palette.error.main }}
+                  onClick={() => replyFriendRequest(username, false)}
+                >
                   <Close />
                 </Button>
               </Box>
