a90e08f8062c25ea4761bb18bd1e1b6a1c9b9522
sossw1 <sossw1@gmail.com>
2023-01-28 22:58:26 -0500

    get socket and close on unmount, on friend request data add to list in state

--- client/src/Components/Friend/Friend.tsx
+++ client/src/Components/Friend/Friend.tsx
@@ -1,8 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
 import FriendRequest from './FriendRequest';
 import FriendRequestList from './FriendRequestList';
 import { useAuth } from '../../Providers/auth';
+import { getSocket } from '../../api/socket';
+
+const socket = getSocket();
 
 export default function Friend() {
   const { user } = useAuth();
@@ -16,6 +19,22 @@ export default function Friend() {
       return next;
     });
   };
+
+  useEffect(() => {
+    if (user) socket.emit('user-data', user);
+
+    socket.on('friend-request', (username: string) => {
+      setFriendRequests((prev) => {
+        const next = [...prev, username];
+        return next;
+      });
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Box display='flex' flexDirection='row' p='2rem'>
