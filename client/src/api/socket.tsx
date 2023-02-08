0bb6cb34c5e04061ce6a6ef66fb1b4b56e5300e2
sossw1 <sossw1@gmail.com>
2023-02-07 21:00:00 -0500

    disconnect socket before tab/window closes to ensure socket id unregistered on user document

--- client/src/api/socket.tsx
+++ client/src/api/socket.tsx
@@ -7,3 +7,7 @@ export const SocketContext = React.createContext(socket);
 export const useSocket = () => {
   return useContext(SocketContext);
 };
+
+window.onbeforeunload = function () {
+  socket.disconnect();
+};
