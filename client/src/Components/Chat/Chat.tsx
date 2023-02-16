5dae106c36a92ebab75c5675a31a5c52e5121d2c
sossw1 <sossw1@gmail.com>
2023-02-15 20:57:20 -0500

    render new friend room upon friend request acceptance

--- client/src/Components/Chat/Chat.tsx
+++ client/src/Components/Chat/Chat.tsx
@@ -50,7 +50,7 @@ export default function Chat() {
   // hooks
   const { user } = useAuth();
   const socket = useSocket();
-  const chat = useChat();
+  const { rooms, loadInitialData, addRoom, newMessage } = useChat();
   // state
   const [drawerOpen, setDrawerOpen] = useState(false);
   const [selectedChatId, setSelectedChatId] = useState<string | null>(null);
@@ -82,7 +82,7 @@ export default function Chat() {
             }
           })
         );
-        chat.loadInitialData({
+        loadInitialData({
           rooms: fetchedRooms,
           roomInvites: user.roomInvites,
           friends: user.friends,
@@ -100,43 +100,46 @@ export default function Chat() {
     }
 
     socket.on('message', (message: IMessageDoc) => {
-      chat.newMessage(message);
+      newMessage(message);
 
       if (isChatComponentMounted.current)
         messageRef?.current?.lastElementChild?.scrollIntoView(true);
     });
 
+    socket.on('friend-request-accepted', (newRoom: IRoomDoc) => {
+      addRoom(newRoom);
+    });
+
     return () => {
       isChatComponentMounted.current = false;
       socket.off('message');
+      socket.off('friend-request-accepted');
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
-    if (selectedChatId === null && chat.rooms.length > 0) {
-      const groups = chat.rooms.filter((room) => !room.isDirect);
+    if (selectedChatId === null && rooms.length > 0) {
+      const groups = rooms.filter((room) => !room.isDirect);
       if (isChatComponentMounted.current) {
         if (groups.length > 0) {
           setSelectedChatId(groups.sort(sortByName)[0]._id);
         } else {
           setSelectedChatId(
-            chat.rooms.sort((a, b) => sortByFriendName(a, b, user))[0]._id
+            rooms.sort((a, b) => sortByFriendName(a, b, user))[0]._id
           );
         }
       }
     }
 
-    const roomSelection = chat.rooms.find(
-      (room) => room._id === selectedChatId
-    );
+    const roomSelection = rooms.find((room) => room._id === selectedChatId);
     if (roomSelection && isChatComponentMounted.current) {
       setSelectedRoom(roomSelection);
       if (user) setSelectedRoomName(getRoomName(roomSelection, user.username));
       setDisplayMessages(selectedRoom ? selectedRoom.messages : []);
       messageRef?.current?.lastElementChild?.scrollIntoView(false);
     }
-  }, [chat, user, selectedChatId, selectedRoom, displayMessages]);
+  }, [rooms, user, selectedChatId, selectedRoom, displayMessages]);
 
   return (
     <Box sx={{ display: 'flex', backgroundColor: theme.palette.grey[100] }}>
@@ -147,7 +150,7 @@ export default function Chat() {
         isChatComponentMounted={isChatComponentMounted}
         selectedChatId={selectedChatId}
         setSelectedChatId={setSelectedChatId}
-        rooms={chat.rooms}
+        rooms={rooms}
         sortByName={sortByName}
         sortByFriendName={sortByFriendName}
       />
