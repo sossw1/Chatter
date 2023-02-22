a858991814c533734d2cedf578e6106fc27feef3
sossw1 <sossw1@gmail.com>
2023-02-21 19:24:23 -0500

    set default color for badge in theme

--- client/src/Providers/theme.tsx
+++ client/src/Providers/theme.tsx
@@ -1,9 +1,28 @@
 import { createTheme } from '@mui/material';
 
+declare module '@mui/material/styles' {
+  interface Palette {
+    neutral: Palette['primary'];
+  }
+
+  interface PaletteOptions {
+    neutral: PaletteOptions['primary'];
+  }
+}
+
+declare module '@mui/material/Badge' {
+  interface BadgePropsColorOverrides {
+    neutral: true;
+  }
+}
+
 const theme = createTheme({
   palette: {
     primary: {
       main: '#7c5cbf'
+    },
+    neutral: {
+      main: '#bdbdbd'
     }
   }
 });
